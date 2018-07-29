c525ea3814e52ffb799af746decf94929bc7b17b
Mark McDermott <mcdermottsolutions@gmail.com>
2018-07-28 19:28:11 -0500

    created flights & hotels api

--- travel/urls.py
+++ travel/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('webapp.urls'))
+    url('', include('webapp.urls')),
+    url('flights-api', include('webapp.urls')),
+    url('hotels-api', include('webapp.urls'))
 ]
--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^flights-api$', views.flightsApi, name='flights api'),
+    url(r'^hotels-api$', views.hotelsApi, name='hotels api')
 ]
