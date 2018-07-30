b5d486d5182112c5fdefe3cd53daff936af1b218
Andrew Magill <andrew.b.magill@gmail.com>
2018-07-30 08:27:27 -0500

    running through snippets tutorial

--- travel/settings.py
+++ travel/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'webapp',
     'proto',
+    'snippets',
 ]
 
 MIDDLEWARE_CLASSES = [
--- travel/urls.py
+++ travel/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
-from rest_framework import routers
-from proto import views
+# from rest_framework import routers
+# from proto import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
+    url(r'^', include('snippets.urls')),
     url(r'api_auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
