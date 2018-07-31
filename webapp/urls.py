1ad7e2a24ad56c881a59eb308e8edaf1953c123f
Mark McDermott <mcdermottsolutions@gmail.com>
2018-07-31 12:47:51 -0500

    added params to the api

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^flights-api$', views.flightsApi, name='flights api'),
-    url(r'^hotels-api$', views.hotelsApi, name='hotels api')
+    url(r'^flights-api/(?P<fromcity>\w+)$', views.flightsApi, name='flights api'),
+    url(r'^hotels-api/(?P<city>\w+)$', views.hotelsApi, name='hotels api')
+    #url(r'^flights-api$', views.flightsApi, name='flights api'),
+    #url(r'^hotels-api/$', views.hotelsApi, name='hotels api')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Flight, Hotel
 from django.http import JsonResponse
 from django.core import serializers
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home.html', {
@@ -11,12 +11,16 @@ def index(request):
     })
     ### return HttpResponse("<h2>Hey</h2>")
 
-def flightsApi(request):
-    flights = Flight.objects.all()
+def flightsApi(request,fromcity):
+    #flights = Flight.objects.all()
+    flights = Flight.objects.filter(depart_city=fromcity)
     flights_serialized = serializers.serialize('json', flights)
     return JsonResponse(flights_serialized, safe=False)
 
-def hotelsApi(request):
-    hotels = Hotel.objects.all()
+def hotelsApi(request,city):
+    #city = city.lower()
+    #hotels = Hotel.objects.all()
+    hotels = Hotel.objects.filter(city=city)
     hotels_serialized = serializers.serialize('json', hotels)
+    #return HttpResponse(city)
     return JsonResponse(hotels_serialized, safe=False)
