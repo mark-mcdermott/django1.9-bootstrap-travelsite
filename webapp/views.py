2786ebd2e79de7473794b83eb9e18e9bd9915171
Mark McDermott <mcdermottsolutions@gmail.com>
2018-08-01 06:47:10 -0500

    changed hotel & flights api to take querystring params

--- webapp/views.py
+++ webapp/views.py
@@ -9,18 +9,38 @@ def index(request):
          'flights': Flight.objects.all(),
          'hotels': Hotel.objects.all()
     })
-    ### return HttpResponse("<h2>Hey</h2>")
 
-def flightsApi(request,fromcity):
-    #flights = Flight.objects.all()
-    flights = Flight.objects.filter(depart_city=fromcity)
-    flights_serialized = serializers.serialize('json', flights)
-    return JsonResponse(flights_serialized, safe=False)
+def flightsApi(request):
+    if request.method == "GET":
+        fromcity = request.GET.get('fromcity', '')
+        tocity = request.GET.get('tocity', '')
+        if fromcity is '' and tocity is '':
+            flights = Flight.objects.all()
+        elif fromcity is not '' and tocity is not '':
+            flights = Flight.objects.filter(depart_city__iexact=fromcity,arrive_city__iexact=tocity)
+        elif fromcity is '':
+            flights = Flight.objects.filter(arrive_city__iexact=tocity)
+        elif tocity is '':
+            flights = Flight.objects.filter(depart_city__iexact=fromcity)
+        if not flights:
+            return HttpResponse("no matches in database")
+        else:
+            flights_serialized = serializers.serialize('json', flights)
+            return JsonResponse(flights_serialized, safe=False)
+    elif request.method == "POST":
+        return HttpResponse("post request")
 
-def hotelsApi(request,city):
-    #city = city.lower()
-    #hotels = Hotel.objects.all()
-    hotels = Hotel.objects.filter(city=city)
-    hotels_serialized = serializers.serialize('json', hotels)
-    #return HttpResponse(city)
-    return JsonResponse(hotels_serialized, safe=False)
+def hotelsApi(request):
+    if request.method == "GET":
+        city = request.GET.get('city', '')
+        if city is '':
+            hotels = Hotel.objects.all()
+        else:
+            hotels = Hotel.objects.filter(city__iexact=city)
+        if not hotels:
+            return HttpResponse("no matches in database")
+        else:
+            hotels_serialized = serializers.serialize('json', hotels)
+            return JsonResponse(hotels_serialized, safe=False)
+    elif request.method == "POST":
+        return HttpResponse("post request")
