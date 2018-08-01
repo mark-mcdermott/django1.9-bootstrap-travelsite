750e1fcc7520498745a7173d4c2f3f467a0d8db4
Mark McDermott <mcdermottsolutions@gmail.com>
2018-08-01 07:14:49 -0500

    returning empty json object if no matches instead of http response with error string

--- webapp/views.py
+++ webapp/views.py
@@ -16,17 +16,10 @@ def flightsApi(request):
         tocity = request.GET.get('tocity', '')
         if fromcity is '' and tocity is '':
             flights = Flight.objects.all()
-        elif fromcity is not '' and tocity is not '':
-            flights = Flight.objects.filter(depart_city__iexact=fromcity,arrive_city__iexact=tocity)
-        elif fromcity is '':
-            flights = Flight.objects.filter(arrive_city__iexact=tocity)
-        elif tocity is '':
-            flights = Flight.objects.filter(depart_city__iexact=fromcity)
-        if not flights:
-            return HttpResponse("no matches in database")
         else:
-            flights_serialized = serializers.serialize('json', flights)
-            return JsonResponse(flights_serialized, safe=False)
+            flights = Flight.objects.filter(depart_city__iexact=fromcity,arrive_city__iexact=tocity)
+        flights_serialized = serializers.serialize('json', flights)
+        return JsonResponse(flights_serialized, safe=False)
     elif request.method == "POST":
         return HttpResponse("post request")
 
@@ -37,10 +30,7 @@ def hotelsApi(request):
             hotels = Hotel.objects.all()
         else:
             hotels = Hotel.objects.filter(city__iexact=city)
-        if not hotels:
-            return HttpResponse("no matches in database")
-        else:
-            hotels_serialized = serializers.serialize('json', hotels)
-            return JsonResponse(hotels_serialized, safe=False)
+        hotels_serialized = serializers.serialize('json', hotels)
+        return JsonResponse(hotels_serialized, safe=False)
     elif request.method == "POST":
         return HttpResponse("post request")
