ce9b6b10eb90cee6691ed1987c29c5afd2bc444e
Harika <harika@Galis-MacBook-Pro.local>
2018-08-05 11:52:55 -0500

    deals booking

--- frontend/src/Deals/index.js
+++ frontend/src/Deals/index.js
@@ -36,6 +36,47 @@ const styles = theme => ({
       this.getResults();
     }
 
+    bookFlight = (flightDetails) => {
+      const { user } = this.props;
+      console.log(flightDetails);
+      var bodyFormData = new FormData();
+      bodyFormData.set('cust_name', 'Mark McDermott');
+      bodyFormData.set('airline_name', 'Delta');
+      bodyFormData.set('credit_type', 'Visa');
+      bodyFormData.set('credit_name', 'Mark McDermott');
+      bodyFormData.set('credit_number', '1234567890123456');
+      bodyFormData.set('credit_expiration', '2018-11-30 11:01');
+      bodyFormData.set('credit_security', '123');
+      bodyFormData.set('depart_city', flightDetails.depart_city);
+      bodyFormData.set('depart_state', flightDetails.depart_state);
+      bodyFormData.set('depart_datetime', flightDetails.depart_datetime);
+      bodyFormData.set('arrive_city', flightDetails.arrive_city);
+      bodyFormData.set('arrive_state', flightDetails.arrive_state);
+      bodyFormData.set('arrive_datetime', flightDetails.arrive_datetime);
+      bodyFormData.set('price_high', flightDetails.price_high);
+      bodyFormData.set('price_low', flightDetails.price_low);
+      axios({
+        method: 'post',
+        url: 'http://localhost:8000/deals-api',
+        data: bodyFormData,
+        config: { headers: { 'Content-Type': 'multipart/form-data' } }
+      })
+        .then((response) => {
+          console.log(response);
+          this.setState({
+            bookingResult: 'Booking completed successfully',
+            bookingStatus: true,
+          })
+        })
+        .catch((error) => {
+          console.log(error);
+          this.setState({
+            bookingResult: 'Booking failed. Please check your details',
+            bookingStatus: true,
+          })
+        });
+    }
+
     getResults = () => {
       // Make a request for a user with a given ID
       axios.get(`http://localhost:8000/deals-api?`)
@@ -56,12 +97,13 @@ const styles = theme => ({
       const { classes } = this.props;
 
       const { searchResults } = this.state;
-
+      const { bookingStatus, bookingResult } = this.state;
       return (
         <div className="tabContent dealsContent">
           <Paper className={classes.root}>
             <div className="dealsDetails">
               <label> Available Deals </label>
+              <p className="bookingResponse">{bookingStatus && bookingResult}</p>
               <Table className={classes.table}>
                 <TableHead>
                   <TableRow>
@@ -73,6 +115,7 @@ const styles = theme => ({
                     <TableCell >Arrival Time</TableCell>
                     <TableCell >Actual Price</TableCell>
                     <TableCell >Deal Price</TableCell>
+                    <TableCell >Book</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -87,9 +130,16 @@ const styles = theme => ({
                         <TableCell >{eachResult.fields.arrive_city}</TableCell>
                         <TableCell >{eachResult.fields.arrive_state}</TableCell>
                         <TableCell >{eachResult.fields.arrive_datetime}</TableCell>
-                        <TableCell >{eachResult.fields.price_high}</TableCell>
-                        <TableCell >{eachResult.fields.price_low}</TableCell>
-
+                        <TableCell >${eachResult.fields.price_high}</TableCell>
+                        <TableCell >${eachResult.fields.price_low}</TableCell>
+                      
+                        <TableCell >
+                        <div className="userFormGroup">
+                          <input type="button" name="submit" id="submit" value="Book"
+                            className="SubmitButton"
+                            onClick={() => this.bookFlight(eachResult.fields)} />
+                        </div>
+                      </TableCell>
                       </TableRow>
                     );
                   })}
