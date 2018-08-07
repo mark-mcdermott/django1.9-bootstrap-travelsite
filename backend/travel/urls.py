84467496c617b70feb509acfb0e59851759ba5a6
Andrew Magill <andrew.b.magill@gmail.com>
2018-08-07 14:27:41 -0500

    merge from master

--- backend/travel/urls.py
+++ backend/travel/urls.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('webapp.urls')),
-    url('flights-api', include('webapp.urls')),
-    url('hotels-api', include('webapp.urls'))
+    url(r'^api/', include('webapp.urls')),
 ]
