0cc1de09d0b74b1c2e87f049d60115fd4630b81a
Andrew Magill <andrew.b.magill@gmail.com>
2018-08-07 17:21:14 -0500

    didn't actually mean to do that

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import AppRoute from './Router';
-
-ReactDOM.render(<AppRoute />, document.getElementById('page'));
